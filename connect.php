304a5ce7504895a1da5c0535b9308b516a81b3dd
janjanuary <janjanuary@users.github.com>
2017-09-18 20:57:15 +0700

    jan

---
+++ connect.php
@@ -0,0 +1,12 @@
+<?php 
+$hostname = "localhost";
+$username = "root";
+$password = "";
+$database = "projecttracking";
+
+$conn = mysqli_connect($hostname,$username,$password,$database) or die("cannot connect DB");
+//$db   = mysql_select_db($database) or die("cannot select DB");
+//mysql_query("SET character_set_results=tis620");
+//mysql_query("SET character_set_client=tis620");
+//mysql_query("SET character_set_connection=tis620");
+?>
